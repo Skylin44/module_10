c07ed2c9dc0d4f474e37d064bf60e4237909b829
Ignatenko Elena <Pox4not@gmail.com>
2024-11-28 11:09:37 +0300

    first commit

---
+++ модуль7/module_7_2.py
@@ -0,0 +1,23 @@
+from pprint import pprint
+
+
+def custom_write(file_name, strings):
+    file_name = 'test.txt'
+    file = open(file_name, 'w', encoding='utf-8')
+    strings_positions = {}
+    for i, string in enumerate(strings):
+        strings_positions[(i + 1, file.tell())] = string
+        file.write(string + '\n')
+    return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
